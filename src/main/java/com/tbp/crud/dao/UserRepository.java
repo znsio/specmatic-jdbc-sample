f7f13cf36c981e835a1c78760a872b5c55f450b6
Joel Rosario <cirquitz@gmail.com>
2023-06-04 19:34:10 +0530

    Removed some files

--- src/main/java/com/tbp/crud/dao/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tbp.crud.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.tbp.crud.entity.User;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-
-}
