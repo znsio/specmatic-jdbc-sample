5f4ea20f70a4ef3a49e604ca45dc388a9702f703
Hari Krishnan <harikrishnan83@gmail.com>
2023-08-08 22:57:29 +0530

    upgrading to specmatic-jdbc 0.65.1-db, simplifying TestDatabaseConfig to a single bean that directly creates DataSource instead of creating a JdbcStubFactory and then creating Datasource bean from that factory bean

--- src/test/java/com/component/products/config/TestDatabaseConfig.java
+++ src/test/java/com/component/products/config/TestDatabaseConfig.java
@@ -1,10 +1,8 @@
 package com.component.products.config;
 
-import in.specmatic.jdbc.stub.DatabaseStub;
 import in.specmatic.jdbc.stub.JdbcStubFactory;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.DependsOn;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 
@@ -16,16 +14,13 @@ import java.io.File;
 @Profile("contract-tests")
 public class TestDatabaseConfig extends DatabaseConfig {
 
+    public static final int DATABASE_STUB_PORT = 9010;
+    public static final String DATABASE_EXPECTATIONS_DIRECTORY = new File("./src/test/resources/db_stub_data").getAbsolutePath();
+
     @Primary
     @Bean
-    @DependsOn("primaryJdbcStubFactory")
-    public DataSource dataSource(JdbcStubFactory jdbcStubFactory) {
-        return jdbcStubFactory.createDataSource();
-    }
-
-    @Bean(destroyMethod = "close")
-    public JdbcStubFactory primaryJdbcStubFactory() {
-        return new JdbcStubFactory(new DatabaseStub("localhost", 9010), new File("./src/test/resources/db_stub_data").getAbsolutePath());
+    public DataSource dataSource() {
+        return new JdbcStubFactory(DATABASE_STUB_PORT, DATABASE_EXPECTATIONS_DIRECTORY).createDataSource();
     }
 
 }
