dc79f7fc8cbf6207e6fbaea1d09d04374a441bd8
Hari Krishnan <harikrishnan83@gmail.com>
2023-08-09 11:01:05 +0530

    removing unnecessary code that was starting the http stub server in ContractTests

--- src/test/java/com/component/products/ContractTests.java
+++ src/test/java/com/component/products/ContractTests.java
@@ -1,22 +1,14 @@
 package com.component.products;
 
 import com.component.products.config.TestDatabaseConfig;
-import in.specmatic.stub.ContractStub;
 import in.specmatic.test.SpecmaticJUnitSupport;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 
-import java.io.IOException;
-
-import static in.specmatic.stub.API.createStub;
-
 @SpringBootTest(classes = ProductsApplication.class, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @ContextConfiguration(classes = {TestDatabaseConfig.class})
 public class ContractTests extends SpecmaticJUnitSupport {
-
-    private static ContractStub stub;
 
     @BeforeAll
     public static void setUp() {
@@ -24,13 +16,6 @@ public class ContractTests extends SpecmaticJUnitSupport {
         System.setProperty("port", "8080");
         System.setProperty("endpointsAPI", "http://localhost:8080/actuator/mappings");
         System.setProperty("spring.profiles.active", "contract-tests");
-        stub =  createStub("localhost", 9000);
     }
 
-    @AfterAll
-    public static void tearDown() throws IOException {
-        if (stub != null) {
-            stub.close();
-        }
-    }
 }
