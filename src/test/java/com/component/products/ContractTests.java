15c29f00eeab8490243fad976e3e31c1477c0953
Hari Krishnan <harikrishnan83@gmail.com>
2024-06-04 09:06:55 +0530

    - Upgrading to Specmatic 1.3.22 - Implementing SpecmaticContractTest interface

--- src/test/java/com/component/products/ContractTests.java
+++ src/test/java/com/component/products/ContractTests.java
@@ -1,14 +1,14 @@
 package com.component.products;
 
 import com.component.products.config.TestDatabaseConfig;
-import in.specmatic.test.SpecmaticJUnitSupport;
+import in.specmatic.test.SpecmaticContractTest;
 import org.junit.jupiter.api.BeforeAll;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest(classes = ProductsApplication.class, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @ContextConfiguration(classes = {TestDatabaseConfig.class})
-public class ContractTests extends SpecmaticJUnitSupport {
+public class ContractTests implements SpecmaticContractTest {
 
     @BeforeAll
     public static void setUp() {
